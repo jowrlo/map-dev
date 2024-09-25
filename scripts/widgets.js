25dab1aff16f4d8914442499d160f63b3f6500bf
jowrlo <125880857+jowrlo@users.noreply.github.com>
2024-09-25 14:54:53 +0100

    Add buttons for js test

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -1,10 +1,18 @@
 const logo = document.querySelector('#logo');
 const widgets = document.querySelector('#widget-box');
 const title = document.querySelector('#title');
+const button1 = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
 
 function openWidgets() {
     widgets.style.display = "block";
     title.innerText = "Updated";
 }
 
-logo.onclick = openWidgets();
+function closeWidgets() {
+    widgets.style.display = "none";
+    title.innerText = "GIS Test";
+}
+
+button1.onclick = openWidgets();
+button2.onclick = closeWidgets();
