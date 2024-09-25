e5d7b1ce234ec76cddb684f5a26b724c489f904c
jowrlo <125880857+jowrlo@users.noreply.github.com>
2024-09-25 10:54:53 +0100

    Update js for test click

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -1,8 +1,10 @@
 const apps = document.querySelector('.material-icons');
-const widgets = document.querySelector('#widget-box')
+const widgets = document.querySelector('#widget-box');
+const title = document.querySelector('#title');
 
 function openWidgets() {
     widgets.style.display = "block";
+    title.innerText = "Updated";
 }
 
 apps.onclick = openWidgets();
